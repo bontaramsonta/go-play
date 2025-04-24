5660bacea436dbafef6c9d97d20bf8e8dcef8d94
souravdhar <bontaramsonta@gmail.com>
2025-04-24 19:12:44 +0530

    bootdev(interfaces): L10 Type assertions

--- main.go
+++ main.go
@@ -1,36 +1,63 @@
 package main
 
-import "fmt"
-
-func (e email) cost() int {
-	cost := 0
-	if e.isSubscribed {
-		cost = 2
-	} else {
-		cost = 5
+// this switch on type is facinating
+func getExpenseReportSwitch(e expense) (string, float64) {
+	switch obj := e.(type) {
+	case email:
+		return obj.toAddress, obj.cost()
+	case sms:
+		return obj.toPhoneNumber, obj.cost()
+	default:
+		return "", 0.0
 	}
-	return len(e.body) * cost
 }
 
-func (e email) format() string {
-	subscriptionText := ""
-	if e.isSubscribed {
-		subscriptionText = "Subscribed"
-	} else {
-		subscriptionText = "Not Subscribed"
+func getExpenseReport(e expense) (string, float64) {
+	emailObj, ok := e.(email)
+	if ok {
+		return emailObj.toAddress, emailObj.cost()
 	}
-	return fmt.Sprintf("'%s' | %s", e.body, subscriptionText)
+	smsObj, ok := e.(sms)
+	if ok {
+		return smsObj.toPhoneNumber, smsObj.cost()
+	}
+	return "", 0.0
 }
+
+// don't touch below this line
 
 type expense interface {
-	cost() int
-}
-
-type formatter interface {
-	format() string
+	cost() float64
 }
 
 type email struct {
 	isSubscribed bool
 	body         string
+	toAddress    string
+}
+
+type sms struct {
+	isSubscribed  bool
+	body          string
+	toPhoneNumber string
+}
+
+type invalid struct{}
+
+func (e email) cost() float64 {
+	if !e.isSubscribed {
+		return float64(len(e.body)) * .05
+	}
+	return float64(len(e.body)) * .01
+}
+
+func (s sms) cost() float64 {
+	if !s.isSubscribed {
+		return float64(len(s.body)) * .1
+	}
+	return float64(len(s.body)) * .03
+}
+
+func (i invalid) cost() float64 {
+	return 0.0
 }
