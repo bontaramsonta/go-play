3346c00656534592bfad7e9a76065c4e2362613d
souravdhar <bontaramsonta@gmail.com>
2025-04-27 16:51:18 +0530

    bootdev(generics): confusing interface generics

--- main/main_test.go
+++ main/main_test.go
@@ -5,35 +5,44 @@ import (
 	"testing"
 )
 
-func TestProcessMessages(t *testing.T) {
+func TestOrgBilling(t *testing.T) {
 	type testCase struct {
-		messages []string
-		expect   []string
+		biller         orgBiller
+		customer       org
+		expectedAmount float64
+		expectedEmail  string
 	}
 
 	runCases := []testCase{
 		{
-			messages: []string{"Sunlit", "Man"},
-			expect:   []string{"Man-processed", "Sunlit-processed"},
+			biller: orgBiller{Plan: "pro"},
+			customer: org{
+				Admin: user{UserEmail: "jaskier@oxenfurt.com"},
+				Name:  "Oxenfurt",
+			},
+			expectedAmount: 3000,
+			expectedEmail:  "jaskier@oxenfurt.com",
 		},
 		{
-			messages: []string{"Nomad do you copy?"},
-			expect:   []string{"Nomad do you copy?-processed"},
-		},
-		{
-			messages: []string{"Scadriel", "Roshar", "Sel", "Nalthis", "Taldain"},
-			expect:   []string{"Taldain-processed", "Roshar-processed", "Sel-processed", "Nalthis-processed", "Scadriel-processed"},
+			biller: orgBiller{Plan: "basic"},
+			customer: org{
+				Admin: user{UserEmail: "vernon@temeria.com"},
+				Name:  "Temeria",
+			},
+			expectedAmount: 2000,
+			expectedEmail:  "vernon@temeria.com",
 		},
 	}
 
 	submitCases := append(runCases, []testCase{
 		{
-			messages: []string{},
-			expect:   []string{},
-		},
-		{
-			messages: []string{"Scadriel"},
-			expect:   []string{"Scadriel-processed"},
+			biller: orgBiller{Plan: "pro"},
+			customer: org{
+				Admin: user{UserEmail: "fringilla@nilfgaard.com"},
+				Name:  "Nilfgaard",
+			},
+			expectedAmount: 3000,
+			expectedEmail:  "fringilla@nilfgaard.com",
 		},
 	}...)
 
@@ -43,55 +52,134 @@ func TestProcessMessages(t *testing.T) {
 	}
 
 	skipped := len(submitCases) - len(testCases)
-
 	passCount := 0
 	failCount := 0
 
-	for _, test := range testCases {
-		fail := false
-		result := processMessages(test.messages)
-
-		if len(result) != len(test.expect) {
-			fail = true
-		}
-
-		counts := make(map[string]int)
-		for _, res := range result {
-			counts[res]++
-		}
-		for _, exp := range test.expect {
-			counts[exp]--
-			if counts[exp] < 0 {
-				fail = true
-			}
-		}
-
-		if fail {
+	for i, test := range testCases {
+		err := testBiller(test.biller, test.customer, test.expectedAmount, test.expectedEmail)
+		if err != nil {
 			failCount++
 			t.Errorf(`---------------------------------
-Test Failed:
-  inputs:   %v
-  expected: %v
-  actual:   %v
-  `, test.messages, test.expect, result)
+OrgTest %d Failed:
+%v
+`, i, err)
 		} else {
 			passCount++
 			fmt.Printf(`---------------------------------
-Test Passed:
-  inputs:   %v
-  expected: %v
-  actual:   %v
-`, test.messages, test.expect, result)
+OrgTest %d Passed:
+  biller:   %v
+  customer: %v
+`, i, test.biller, test.customer)
 		}
 	}
 
 	fmt.Println("---------------------------------")
 	if skipped > 0 {
-		fmt.Printf("%d passed, %d failed, %d skipped\n", passCount, failCount, skipped)
+		fmt.Printf("OrgBilling: %d passed, %d failed, %d skipped\n", passCount, failCount, skipped)
 	} else {
-		fmt.Printf("%d passed, %d failed\n", passCount, failCount)
+		fmt.Printf("OrgBilling: %d passed, %d failed\n", passCount, failCount)
+	}
+}
+
+func TestUserBilling(t *testing.T) {
+	type testCase struct {
+		biller         userBiller
+		customer       user
+		expectedAmount float64
+		expectedEmail  string
 	}
 
+	runCases := []testCase{
+		{
+			biller:         userBiller{Plan: "basic"},
+			customer:       user{UserEmail: "vesemir@kaermorhen.com"},
+			expectedAmount: 50,
+			expectedEmail:  "vesemir@kaermorhen.com",
+		},
+		{
+			biller:         userBiller{Plan: "pro"},
+			customer:       user{UserEmail: "zoltan@mahakam.com"},
+			expectedAmount: 100,
+			expectedEmail:  "zoltan@mahakam.com",
+		},
+	}
+
+	submitCases := append(runCases, []testCase{
+		{
+			biller:         userBiller{Plan: "pro"},
+			customer:       user{UserEmail: "extra@submit.com"},
+			expectedAmount: 100,
+			expectedEmail:  "extra@submit.com",
+		},
+	}...)
+
+	testCases := runCases
+	if withSubmit {
+		testCases = submitCases
+	}
+
+	skipped := len(submitCases) - len(testCases)
+	passCount := 0
+	failCount := 0
+
+	for i, test := range testCases {
+		err := testBiller(test.biller, test.customer, test.expectedAmount, test.expectedEmail)
+		if err != nil {
+			failCount++
+			t.Errorf(`---------------------------------
+UserTest %d Failed:
+%v
+`, i, err)
+		} else {
+			passCount++
+			fmt.Printf(`---------------------------------
+UserTest %d Passed:
+  biller:   %v
+  customer: %v
+`, i, test.biller, test.customer)
+		}
+	}
+
+	fmt.Println("---------------------------------")
+	if skipped > 0 {
+		fmt.Printf("UserBilling: %d passed, %d failed, %d skipped\n", passCount, failCount, skipped)
+	} else {
+		fmt.Printf("UserBilling: %d passed, %d failed\n", passCount, failCount)
+	}
+}
+
+func testBiller[C customer](
+	b biller[C],
+	c C,
+	expectedAmount float64,
+	expectedEmail string,
+) error {
+	currentBill := b.Charge(c)
+	name := b.Name()
+
+	if currentBill.Amount != expectedAmount ||
+		currentBill.Customer.GetBillingEmail() != expectedEmail {
+		return fmt.Errorf(`biller "%v" FAILED:
+  biller Type:     %T
+  customer Type:   %T
+  customer:        %v
+  expected amount: %v
+  expected email:  %v
+  actual amount:   %v
+  actual email:    %v
+`,
+			name,
+			b,
+			c,
+			c,
+			expectedAmount,
+			expectedEmail,
+			currentBill.Amount,
+			currentBill.Customer.GetBillingEmail(),
+		)
+	}
+
+	return nil
 }
 
 // withSubmit is set at compile time depending
