a35582ec90d8cb28ec8a719a986485f5f266ebfb
souravdhar <bontaramsonta@gmail.com>
2025-07-07 22:08:21 +0530

    leetcode solve

--- main.go
+++ main.go
@@ -1,114 +1,120 @@
 package main
 
 import (
-	"math/rand"
-	"sort"
+	"fmt"
 )
 
-func hIndex(citations []int) int {
-	sort.Sort(sort.Reverse(sort.IntSlice(citations)))
-	for i, citation := range citations {
-		if citation < i+1 {
-			return i
+func candy(ratings []int) int {
+	result := make([]int, len(ratings))
+
+	for i := range ratings {
+		result[i] = 1
+
+		if i > 0 && ratings[i] > ratings[i-1] {
+			result[i] = result[i-1] + 1
 		}
 	}
-	return len(citations)
-}
+	fmt.Printf("1st Pass Result: %v\n", result)
 
-type RandomizedSet struct {
-	set map[int]int8
-}
-
-func Constructor() RandomizedSet {
-	return RandomizedSet{
-		set: make(map[int]int8),
+	for i := len(ratings) - 2; i >= 0; i-- {
+		if ratings[i] > ratings[i+1] {
+			result[i] = max(result[i], result[i+1]+1)
+		}
 	}
+	fmt.Printf("2nd Pass Result: %v\n", result)
+
+	sum := 0
+	for _, v := range result {
+		sum += v
+	}
+	return sum
 }
 
-func (this *RandomizedSet) Insert(val int) bool {
-	if _, ok := this.set[val]; ok {
-		return false
-	} else {
-		this.set[val] = 1
-		return true
+func trap(height []int) int {
+	prefix := make([]int, len(height))
+	suffix := make([]int, len(height))
+
+	current := height[0]
+
+	for i := 1; i < len(height); i++ {
+		if height[i] > current {
+			current = height[i]
+		}
+		prefix[i] = current
 	}
+
+	fmt.Printf("Prefix: %v\n", prefix)
+
+	current = height[len(height)-1]
+	for i := len(height) - 2; i >= 0; i-- {
+		if height[i] > current {
+			current = height[i]
+		}
+		suffix[i] = current
+	}
+
+	fmt.Printf("Suffix: %v\n", suffix)
+
+	sum := 0
+	for i := 0; i < len(height); i++ {
+		sum += max(0, min(prefix[i], suffix[i])-height[i])
+	}
+
+	return sum
 }
 
-func (this *RandomizedSet) Remove(val int) bool {
-	if _, ok := this.set[val]; ok {
-		delete(this.set, val)
-		return true
-	} else {
-		return false
-	}
+type RomanNumeralValue struct {
+	order      int
+	decimalVal int
 }
 
-func (this *RandomizedSet) GetRandom() int {
-	if len(this.set) == 0 {
-		return 0
+func romanToInt(s string) int {
+	romanValue := map[byte]RomanNumeralValue{
+		'I': RomanNumeralValue{order: 1, decimalVal: 1},
+		'V': RomanNumeralValue{order: 2, decimalVal: 5},
+		'X': RomanNumeralValue{order: 3, decimalVal: 10},
+		'L': RomanNumeralValue{order: 4, decimalVal: 50},
+		'C': RomanNumeralValue{order: 5, decimalVal: 100},
+		'D': RomanNumeralValue{order: 6, decimalVal: 500},
+		'M': RomanNumeralValue{order: 7, decimalVal: 1000},
 	}
-	keys := make([]int, 0, len(this.set))
-	for k := range this.set {
-		keys = append(keys, k)
-	}
-	return keys[rand.Intn(len(keys))]
-}
 
-func productExceptSelf(nums []int) []int {
-	product := 1
-	countZero := 0
-	for _, num := range nums {
-		if num != 0 {
-			product = product * num
+	sum := 0
+	for i := 0; i < len(s); i++ {
+		if i != len(s)-1 && romanValue[s[i]].order < romanValue[s[i+1]].order {
+			sum -= romanValue[s[i]].decimalVal
 		} else {
-			countZero++
+			sum += romanValue[s[i]].decimalVal
 		}
 	}
-	answers := make([]int, len(nums))
-	if countZero > 1 {
-		return answers
-	}
-	for i, num := range nums {
-		if num == 0 {
-			answers[i] = product
-			return append(make([]int, i+1), answers[i:]...)
-		}
-		answers[i] = product / num
-	}
-	return answers
+	return sum
 }
 
 func main() {
-	// ["RandomizedSet","insert","remove","insert","getRandom","remove","insert","getRandom"]
-	// [[],[1],[2],[2],[],[1],[2],[]]
+	var ratings []int
 
-	obj := Constructor()
+	ratings = []int{1, 0, 2}
+	fmt.Printf("For %v : %d\n", ratings, candy(ratings))
 
-	// insert(1)
-	result1 := obj.Insert(1)
-	println("insert(1):", result1)
+	ratings = []int{1, 2, 2}
+	fmt.Printf("For %v : %d\n", ratings, candy(ratings))
 
-	// remove(2)
-	result2 := obj.Remove(2)
-	println("remove(2):", result2)
+	var height []int
 
-	// insert(2)
-	result3 := obj.Insert(2)
-	println("insert(2):", result3)
+	height = []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
+	fmt.Printf("For %v : %d\n", height, trap(height))
 
-	// getRandom()
-	result4 := obj.GetRandom()
-	println("getRandom():", result4)
+	height = []int{4, 2, 0, 3, 2, 5}
+	fmt.Printf("For %v : %d\n", height, trap(height))
 
-	// remove(1)
-	result5 := obj.Remove(1)
-	println("remove(1):", result5)
+	var s string
 
-	// insert(2)
-	result6 := obj.Insert(2)
-	println("insert(2):", result6)
+	s = "III"
+	fmt.Printf("For %s Roman: %d\n", s, romanToInt(s))
 
-	// getRandom()
-	result7 := obj.GetRandom()
-	println("getRandom():", result7)
+	s = "LVIII"
+	fmt.Printf("For %s Roman: %d\n", s, romanToInt(s))
+
+	s = "MCMXCIV"
+	fmt.Printf("For %s Roman: %d\n", s, romanToInt(s))
 }
