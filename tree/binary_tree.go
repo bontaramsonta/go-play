2fcfd34f907d5e6237d66ecc02bba17f75029254
souravdhar <bontaramsonta@gmail.com>
2025-10-20 14:20:32 +0530

    binary tree operations

--- tree/binary_tree.go
+++ tree/binary_tree.go
@@ -1,10 +1,28 @@
 package tree
 
+import "math"
+
 type Tree interface {
 	Root() *Node[int]
-	levelOrder() []int
 	InsertLeft(parent *Node[int], value int) *Node[int]
 	InsertRight(parent *Node[int], value int) *Node[int]
+	levelOrder() []int
+	GetSize() int
+	GetHeight() int
+}
+
+type PrintAbleTree interface {
+	PrintLeftView() []int
+	PrintRightView() []int
+	PrintBottomView() []int
+}
+
+type SumPropertyTree interface {
+	HasSumProperty() bool
+}
+
+type BalancedTree interface {
+	isBalanced() bool
 }
 
 type Node[T any] struct {
@@ -59,6 +77,189 @@ func (b *BinaryTree) levelOrder() []int {
 		}
 	}
 	return result
+}
+
+func (b *BinaryTree) GetSize() int {
+	if b.root == nil {
+		return 0
+	}
+	queue := []*Node[int]{b.root}
+	for i := 0; i < len(queue); i++ {
+		current := queue[i]
+		if current.left != nil {
+			queue = append(queue, current.left)
+		}
+		if current.right != nil {
+			queue = append(queue, current.right)
+		}
+	}
+	return len(queue)
+}
+
+func (b *BinaryTree) GetHeight() int {
+	if b.root == nil {
+		return 0
+	}
+	type QueueItem struct {
+		node  *Node[int]
+		level int
+	}
+	queue := []QueueItem{{b.root, 1}}
+	maxHeight := 0
+	for i := 0; i < len(queue); i++ {
+		current := queue[i]
+		if current.level > maxHeight {
+			maxHeight = current.level
+		}
+		if current.node.left != nil {
+			queue = append(queue, QueueItem{current.node.left, current.level + 1})
+		}
+		if current.node.right != nil {
+			queue = append(queue, QueueItem{current.node.right, current.level + 1})
+		}
+	}
+	return maxHeight
+}
+
+func (b *BinaryTree) PrintLeftView() []int {
+	var result []int
+	if b.root == nil {
+		return result
+	}
+
+	type QueueItem struct {
+		node  *Node[int]
+		level int
+	}
+	var queue []QueueItem
+	queue = append(queue, QueueItem{b.root, 0})
+	previousLevel := -1
+	for i := 0; i < len(queue); i++ {
+		current := queue[i]
+		if current.level > previousLevel {
+			result = append(result, current.node.value)
+			previousLevel = current.level
+		}
+
+		if current.node.left != nil {
+			queue = append(queue, QueueItem{current.node.left, current.level + 1})
+		}
+		if current.node.right != nil {
+			queue = append(queue, QueueItem{current.node.right, current.level + 1})
+		}
+	}
+	return result
+}
+
+func (b *BinaryTree) PrintRightView() []int {
+	var result []int
+	if b.root == nil {
+		return result
+	}
+
+	type QueueItem struct {
+		node  *Node[int]
+		level int
+	}
+	var queue []QueueItem
+	queue = append(queue, QueueItem{b.root, 0})
+	previousLevel := -1
+	for i := 0; i < len(queue); i++ {
+		current := queue[i]
+		if current.level > previousLevel {
+			result = append(result, current.node.value)
+			previousLevel = current.level
+		}
+
+		if current.node.right != nil {
+			queue = append(queue, QueueItem{current.node.right, current.level + 1})
+		}
+		if current.node.left != nil {
+			queue = append(queue, QueueItem{current.node.left, current.level + 1})
+		}
+	}
+	return result
+}
+
+func (b *BinaryTree) PrintBottomView() []int {
+	if b.root == nil {
+		return []int{}
+	}
+
+	type QueueItem struct {
+		node       *Node[int]
+		horizontal int
+	}
+	queue := []QueueItem{{b.root, 0}}
+	horizontalMap := make(map[int]int)
+	minHorizontal, maxHorizontal := 0, 0
+	for i := 0; i < len(queue); i++ {
+		current := queue[i]
+		horizontalMap[current.horizontal] = current.node.value
+
+		if current.node.left != nil {
+			queue = append(queue, QueueItem{current.node.left, current.horizontal - 1})
+		}
+		if current.node.right != nil {
+			queue = append(queue, QueueItem{current.node.right, current.horizontal + 1})
+		}
+		minHorizontal = min(minHorizontal, current.horizontal)
+		maxHorizontal = max(maxHorizontal, current.horizontal)
+	}
+
+	var result []int
+	for h := minHorizontal; h <= maxHorizontal; h++ {
+		result = append(result, horizontalMap[h])
+	}
+	return result
+}
+
+func (b *BinaryTree) HasSumProperty() bool {
+	var checkSumProperty func(node *Node[int]) bool
+	checkSumProperty = func(node *Node[int]) bool {
+		if node == nil {
+			return true
+		}
+		if node.left == nil && node.right == nil {
+			return true
+		}
+
+		sum := 0
+		if node.left != nil {
+			sum += node.left.value
+		}
+		if node.right != nil {
+			sum += node.right.value
+		}
+
+		if node.value != sum {
+			return false
+		}
+
+		return checkSumProperty(node.left) && checkSumProperty(node.right)
+	}
+
+	return checkSumProperty(b.root)
+}
+
+func (b *BinaryTree) isBalanced() bool {
+	var checkBalance func(node *Node[int]) (bool, int)
+	checkBalance = func(node *Node[int]) (bool, int) {
+		if node == nil {
+			return true, 0
+		}
+
+		leftBalanced, leftHeight := checkBalance(node.left)
+		rightBalanced, rightHeight := checkBalance(node.right)
+
+		currentBalanced := leftBalanced && rightBalanced && math.Abs(float64(leftHeight-rightHeight)) <= 1
+		currentHeight := max(leftHeight, rightHeight) + 1
+
+		return currentBalanced, currentHeight
+	}
+
+	balanced, _ := checkBalance(b.root)
+	return balanced
 }
 
 func NewBinaryTree(rootValue int) *BinaryTree {
--- tree/binary_tree_test.go
+++ tree/binary_tree_test.go
@@ -34,3 +34,111 @@ func TestInsertWithMoreThanCount(t *testing.T) {
 		t.Errorf("Inserted more but still got %v, want %v", moreThanLast, nil)
 	}
 }
+
+func TestBinaryTreeSize(t *testing.T) {
+	tree := NewBinaryTree(1)
+	left := tree.InsertLeft(tree.Root(), 2)
+	tree.InsertRight(tree.Root(), 3)
+	secondLeft := tree.InsertLeft(left, 4)      // second left
+	thirdLeft := tree.InsertLeft(secondLeft, 5) // third left
+	tree.InsertLeft(thirdLeft, 6)               // forth left
+
+	expectedSize := 6
+	resultSize := tree.GetSize()
+
+	if resultSize != expectedSize {
+		t.Errorf("got size %d, want %d", resultSize, expectedSize)
+	}
+}
+
+func TestBinaryTreePrintLeftView(t *testing.T) {
+	tree := NewBinaryTree(1)
+	left := tree.InsertLeft(tree.Root(), 2)
+	right := tree.InsertRight(tree.Root(), 3)
+	tree.InsertLeft(left, 4)
+	tree.InsertRight(left, 5)
+	tree.InsertLeft(right, 6)
+	tree.InsertRight(right, 7)
+
+	expectedLeftView := []int{1, 2, 4}
+	resultLeftView := tree.PrintLeftView()
+
+	if !slices.Equal(resultLeftView, expectedLeftView) {
+		t.Errorf("got left view %v, want %v", resultLeftView, expectedLeftView)
+	}
+}
+
+func TestBinaryTreeHeight(t *testing.T) {
+	tree := NewBinaryTree(1)
+	left := tree.InsertLeft(tree.Root(), 2)
+	tree.InsertRight(tree.Root(), 3)
+	secondLeft := tree.InsertLeft(left, 4)      // second left
+	thirdLeft := tree.InsertLeft(secondLeft, 5) // third left
+	tree.InsertLeft(thirdLeft, 6)               // forth left
+
+	expectedHeight := 5
+	resultHeight := tree.GetHeight()
+
+	if resultHeight != expectedHeight {
+		t.Errorf("got height %d, want %d", resultHeight, expectedHeight)
+	}
+}
+
+func TestBinaryTreePrintBottom(t *testing.T) {
+	tree := NewBinaryTree(1)
+	left := tree.InsertLeft(tree.Root(), 2)
+	right := tree.InsertRight(tree.Root(), 3)
+	tree.InsertLeft(left, 4)
+	tree.InsertRight(left, 5)
+	tree.InsertLeft(right, 6)
+	tree.InsertRight(right, 7)
+
+	expectedBottomView := []int{4, 5, 6, 7}
+	resultBottomView := tree.PrintBottomView()
+
+	if !slices.Equal(resultBottomView, expectedBottomView) {
+		t.Errorf("got bottom view %v, want %v", resultBottomView, expectedBottomView)
+	}
+}
+
+func TestBinaryTreeHasSumProperty(t *testing.T) {
+	tree := NewBinaryTree(10)
+	left := tree.InsertLeft(tree.Root(), 8)
+	right := tree.InsertRight(tree.Root(), 2)
+	tree.InsertLeft(left, 3)
+	tree.InsertRight(left, 5)
+	tree.InsertLeft(right, 2)
+
+	if !tree.HasSumProperty() {
+		t.Errorf("expected tree to have sum property")
+	}
+
+	// Modify tree to violate sum property
+	right.left.value = 3
+
+	if tree.HasSumProperty() {
+		t.Errorf("expected tree to not have sum property")
+	}
+}
+
+func TestBinaryTreeIsBalanced(t *testing.T) {
+	tree := NewBinaryTree(1)
+	left := tree.InsertLeft(tree.Root(), 2)
+	right := tree.InsertRight(tree.Root(), 3)
+	tree.InsertLeft(left, 4)
+	tree.InsertRight(left, 5)
+	lastLeft := tree.InsertLeft(right, 6)
+	tree.InsertRight(right, 7)
+
+	if !tree.isBalanced() {
+		t.Errorf("expected tree to be balanced")
+	}
+
+	// Add extra nodes to make it unbalanced
+	extraLeft := tree.InsertLeft(lastLeft, 8)
+	tree.InsertLeft(extraLeft, 9)
+
+	if tree.isBalanced() {
+		t.Errorf("expected tree to be unbalanced")
+	}
+}
